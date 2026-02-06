a32ce5861f2742bf524862bff3ef6826b3e235c3
Timité Ahmed <hamstrongtimite@gmail.com>
2026-02-06 23:46:17 +0000

    Fix home page error when database is empty

--- core/views.py
+++ core/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from boutique.models import Category, Product
 
 def home(request):
-    categories = Category.objects.all()
-    products = Product.objects.all()[:8]  # produits populaires
+    categories = Category.objects.all() if Category.objects.exists() else []
+    products = Product.objects.all()[:8] if Product.objects.exists() else []
 
     return render(request, "home.html", {
         "categories": categories,
         "products": products,
     })
+
 
 def about(request):
     return render(request, 'about.html')
