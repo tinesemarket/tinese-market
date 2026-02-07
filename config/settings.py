5c5064119c7e4056a250ee15298eee947b58508a
Timité Ahmed <hamstrongtimite@gmail.com>
2026-02-07 10:31:42 +0000

    Fix DATABASE_URL for Render PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -89,7 +89,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
