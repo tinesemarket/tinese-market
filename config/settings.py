53ff5e44c76b89c1ab84d28a5590cdf79d154922
Timité Ahmed <hamstrongtimite@gmail.com>
2026-02-07 11:13:30 +0000

    Fix database configuration for Render PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -87,13 +87,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+    'default': dj_database_url.parse(
+        os.environ.get(
+            'DATABASE_URL',
+            f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        ),
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=not DEBUG
     )
 }
+
+
+
 
 
 
